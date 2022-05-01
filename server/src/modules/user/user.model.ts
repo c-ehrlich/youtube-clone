419ab628094ddc416e073d7759650484e32ac1d4
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-05-01 10:52:42 +0900

    Create user model

---
+++ server/src/modules/user/user.model.ts
@@ -0,0 +1,33 @@
+import { getModelForClass, prop, pre } from '@typegoose/typegoose';
+import argon2 from 'argon2';
+
+// hash passwords
+@pre<User>('save', async function (next) {
+  if (this.isModified('password') || this.isNew) {
+    const hash = await argon2.hash(this.password);
+    this.password = hash;
+    return next();
+  }
+})
+
+export class User {
+  @prop({ required: true, unique: true })
+  public username: string;
+
+  @prop({ required: true, unique: true })
+  public email: string;
+
+  @prop({ required: true })
+  public password: string;
+
+  public async comparePassword(password: string): Promise<boolean> {
+    return argon2.verify(this.password, password);
+  }
+}
+
+export const UserModel = getModelForClass(User, {
+  schemaOptions: {
+    // add createdAt and updatedAt
+    timestamps: true,
+  },
+});
