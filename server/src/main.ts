20f44d40ff826f04b44684e53bf99b9ebe0d3662
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-05-01 10:27:01 +0900

    Implement DB Connection and Logger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,21 +1,24 @@
 import express from 'express';
+import { connectToDatabase, disconnectFromDatabase } from './utils/database';
+import logger from './utils/logger';
 
 const PORT = process.env.PORT || 4000;
 const app = express();
-const server = app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+const server = app.listen(PORT, async () => {
+  await connectToDatabase();
+  logger.info(`Server listening at http://localhost:${PORT}`);
 });
 
 // graceful shutdown
 const signals = ['SIGTERM', 'SIGINT'];
 function gracefulShutdown(signal: string) {
   process.on(signal, async () => {
-    console.log('Goodbye, got signal', signal);
+    logger.info('Goodbye, got signal', signal);
     server.close();
 
-    // TODO: disconnect from the db
+    await disconnectFromDatabase();
 
-    console.log('My work here is done');
+    logger.info('My work here is done');
     process.exit(0);
   });
 }
