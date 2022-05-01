64108e6697094e924f06b2cac0ebe138442f64e5
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-05-01 16:50:27 +0900

    feat(FE): Login

--- client/api/index.ts
+++ client/api/index.ts
@@ -1,14 +1,25 @@
-import axios from "axios";
+import axios from 'axios';
 
-const base = process.env.NEXT_PUBLIC_API_ENDPOINT || 'http://MISSING_ENV_VAR_NEXT_PUBLIC_API_ENDPOINT:3000'
+const base =
+  process.env.NEXT_PUBLIC_API_ENDPOINT ||
+  'http://MISSING_ENV_VAR_NEXT_PUBLIC_API_ENDPOINT:3000';
 
-const userBase = `${base}/api/users`
+const userBase = `${base}/api/users`;
+const authBase = `${base}/api/auth`;
 
 export function registerUser(payload: {
-  username: string,
-  email: string,
-  password: string,
-  confirmPassword: string,
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
 }) {
-  return axios.post(userBase, payload).then(res => res.data);
+  return axios.post(userBase, payload).then((res) => res.data);
+}
+
+export function loginUser(payload: { email: string; password: string }) {
+  return axios
+    .post(authBase, payload, {
+      withCredentials: true,
+    })
+    .then((res) => res.data);
 }
--- client/pages/auth/login.tsx
+++ client/pages/auth/login.tsx
@@ -1,5 +1,70 @@
+import {
+  Button,
+  Container,
+  Paper,
+  PasswordInput,
+  Stack,
+  TextInput,
+  Title,
+} from '@mantine/core';
+import { useForm } from '@mantine/hooks';
+import { showNotification, updateNotification } from '@mantine/notifications';
+import { AxiosError } from 'axios';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useMutation } from 'react-query';
+import { loginUser } from '../../api';
+
 function LoginPage() {
-  return <div>Login Page</div>  
+  const router = useRouter();
+
+  const form = useForm({
+    initialValues: {
+      email: '',
+      username: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  const mutation = useMutation<
+    string,
+    AxiosError,
+    Parameters<typeof loginUser>['0']
+  >(loginUser, {
+    onSuccess: () => {
+      router.push('/');
+    },
+  });
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <Container>
+        <Title>Login</Title>
+        <Paper withBorder shadow='md' p={30} mt={30} radius='md'>
+          <form onSubmit={form.onSubmit((values) => mutation.mutate(values))}>
+            <Stack>
+              <TextInput
+                label='Email'
+                placeholder='jane@example.com'
+                required
+                {...form.getInputProps('email')}
+              />
+              <PasswordInput
+                label='Password'
+                placeholder='Your strong password'
+                required
+                {...form.getInputProps('password')}
+              />
+              <Button type='submit'>Login</Button>
+            </Stack>
+          </form>
+        </Paper>
+      </Container>
+    </>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
